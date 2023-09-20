75f6225d6a7644aa9130124519528b7f89da4d19
hugo <hugo@saturncloud.io>
2023-09-20 14:15:07 -0700

    register with fsspec on install

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ install_requires = [
     "requests",
     "marshmallow>=3.10.0,<4.0.0",
     "marshmallow-dataclass",
+    "fsspec"
 ]
 
 
@@ -23,6 +24,9 @@ setup(
     entry_points={
         "console_scripts": [
             "saturnfs=saturnfs.cli.main:entrypoint",
+        ],
+        'fsspec.specs': [
+            'sfs=saturnfs.SaturnFS',
         ],
     },
     url="https://saturncloud.io/",
