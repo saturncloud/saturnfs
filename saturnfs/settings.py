d7f3145b5d904c5158a16ede2dee26529dc7d771
hugo <hugo@saturncloud.io>
2024-03-14 18:24:03 +0000

    add exclude to cli

--- saturnfs/settings.py
+++ saturnfs/settings.py
@@ -5,7 +5,7 @@ from saturnfs.utils import Units
 SATURN_BASE_URL = os.environ["SATURN_BASE_URL"]
 SATURN_TOKEN = os.environ["SATURN_TOKEN"]
 
-S3_MIN_PART_SIZE = 5 * Units.MiB
+S3_MIN_PART_SIZE = 64 * Units.MiB
 S3_MAX_PART_SIZE = 5 * Units.GiB
 S3_MAX_NUM_PARTS = 10000
 OBJECT_STORAGE_MAX_LIST_COUNT = 1000
